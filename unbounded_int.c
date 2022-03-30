61e605c68e06a07d82712a6f37408a1c8879aed8
Mojigot7 <ayyacine.pro@gmail.com>
2022-03-30 11:51:25 +0200

    debut de fonction

--- unbounded_int.c
+++ unbounded_int.c
@@ -5,3 +5,31 @@
 #include <assert.h>
 
 #include "unbounded_int.h"
+
+char *long2char(long long b){
+    char *c = malloc(sizeof(char));
+    assert(c != NULL);
+    
+}
+
+int unbounded_int_cmp_ll(unbounded_int a, long long b){
+    size_t length = sizeof(b)/sizeof(long long);
+    if((a.signe == '-' && b >= 0) || (a.len < length)){
+        return -1;
+    }
+    if((a.signe == '+' && b < 0) || (a.len > length)){
+        return 1;
+    }
+    long long tmp = b/pow(10,length-1);
+    while(a.premier != NULL){
+        if(a.premier->c-'0' < tmp){
+            return -1;
+        }
+        if(a.premier->c-'0' > tmp){
+            return 1;
+        }
+        a.premier = a.premier->suivant;
+        tmp = tmp/pow(10,length-1);
+    }
+    return 0;
+}
