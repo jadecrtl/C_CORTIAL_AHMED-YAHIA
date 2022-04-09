76b2262f4e05e9e6875d2fc9e0ab3fa6ff987b42
jadecrtl <jadecortial@gmail.com>
2022-04-09 12:22:03 +0200

    création des structures variable et liste_variable

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -2,16 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "unbounded_int.h"
+
 FILE *fichier_source;
 FILE *fichier_resultat;
 
+typedef struct variable{
+    char *nom;
+    struct variable *suivant;
+    unbounded_int valeur;
+    struct variable *precedent;
+}variable;
+
+typedef struct {
+    size_t len;
+    variable *premier;
+    variable *dernier;
+}liste_variable;
+
 void recupere_argument(int argc, char *argv[]);
 FILE *ouvrir_fichier_en_lecture(char *nom_fichier);
+FILE *ouvrir_fichier_en_ecriture(char *nom_fichier);
 
 int main(int argc, char *argv[]) {
     printf("hello world calc_unbounded_int\n");
     recupere_argument(argc, argv);
     fclose(fichier_source);
+    fclose(fichier_resultat);
     return 0;
 }
 
@@ -33,6 +50,7 @@ void recupere_argument(int argc, char *argv[]) {
         if (strcmp(argv[1], "-o") == 0) {
             printf("faire stdin\n");
             printf("fichier resultat = %s\n", argv[2]);
+            fichier_resultat = ouvrir_fichier_en_ecriture(argv[2]);
             return;
         }
     }
@@ -41,12 +59,14 @@ void recupere_argument(int argc, char *argv[]) {
             printf("fichier source = %s\n", argv[2]);
             fichier_source = ouvrir_fichier_en_lecture(argv[2]);
             printf("fichier resultat = %s\n", argv[4]);
+            fichier_resultat = ouvrir_fichier_en_ecriture(argv[4]);
             return;
         }
         if (strcmp(argv[1], "-o") == 0 && strcmp(argv[3], "-i") == 0) {
             printf("fichier source = %s\n", argv[4]);
             fichier_source = ouvrir_fichier_en_lecture(argv[4]);
             printf("fichier resultat = %s\n", argv[2]);
+            fichier_resultat = ouvrir_fichier_en_ecriture(argv[2]);
             return;
         }
     }
@@ -66,4 +86,13 @@ FILE *ouvrir_fichier_en_lecture(char *nom_fichier) {
     return fichier;
 }
 
-
+FILE *ouvrir_fichier_en_ecriture(char *nom_fichier) {
+    FILE *fichier = fopen(nom_fichier, "w");
+    if (fichier != NULL) {
+        printf("%s ouvert en écriture\n", nom_fichier);//IMPOSSIBLE SI ON A PAS LES DROITS D'ECRITURE
+    }//POSSIBLE DE L'OUVRIR EN ECRITURE MEME SI ON ECRIT ACTUELLEMENT DEDANS
+    else {
+        printf("impossible d'ouvrir <%s> en écriture\n", nom_fichier);
+    }
+    return fichier;
+}
